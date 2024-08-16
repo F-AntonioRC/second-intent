c37e24e6cec05e0aa3975056a6ab0cfba00f6334
F-AntonioRC <l20920144@voaxaca.tecnm.mx>
2024-08-15 20:23:24 -0600

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://F-AntonioRC.github.io/second-intent'
 })
